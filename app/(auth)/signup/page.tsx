05041ec2979b40e8ed77a08b2671a8134ea33309
Ravi Kumar <ravi@finnspire.com>
2025-04-29 15:43:02 +0530

    authform complete

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+// import AuthForm from "@/components/AuthForm";
 
-export const Page = () => {
-    return (
-        <div>hii</div>
-    )
-}
+const Page = () => {
+  return <AuthForm type="sign-up " />;
+};
+
+export default Page;
